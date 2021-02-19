7e7d7d2ee27d5101d75cd8a44c36946d7358303c
Reshma <reshmashaik327@gmail.com>
2021-02-19 11:12:30 +0530

    Initial commit

---
+++ about.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,c;
+clrscr();
+if(a>b && a>c)
+{
+printf("a is biggest");
+}
+else if(b>a && b>c)
+{
+printf("b is biggest");
+}
+else
+{
+printf("c");
+}
+getch();
+}
